2c11cf7664cf0db41f8b742d5ec3cd939d63648e
schneider <schneider@blinkenlichts.net>
2013-01-12 22:19:27 +0100

    encrypt decrypt packets on the bus

--- software/lc/bus_process.c
+++ software/lc/bus_process.c
@@ -3,9 +3,10 @@
 #include "command_process.h"
 #include "packet.h" 
 #include "config.h"
+#include "aes.h"
+
 #include <util/delay.h>
 #include <string.h>
-
 void bus_init(void)
 {
 }
@@ -21,21 +22,24 @@ void bus_process(void)
     if( channel == NODE_ADDRESS && len == 0 ){
         cmd_new(CMD_SEND_STATE, NULL);
     }else if( channel == NODE_ADDRESS && len == sizeof(packet_t) ){
-        //TODO: decrypt
-        //TODO: check sequence number
-        packet_t *p = (packet_t *)bus_getMessage();
-        cmd_new(p->cmd, p->data);
+        uint8_t *msg = bus_getMessage();
+        aes_decrypt(msg);
+        packet_t *packet = (packet_t *) msg;
+        if( packet_check(packet) ){
+            //TODO: check sequence number
+            cmd_new(packet->cmd, packet->data);
+        }
     }
 }
 
 void bus_sendPacket(packet_t *p)
 {
-    memcpy(p->magic, "SESAME", 6);
+    memcpy(p->magic, PACKET_MAGIC, sizeof(p->magic));
     //TODO: increment sequence number
-    //TODO: encrypt
     p->seq = 0;
-    _delay_ms(1);
-    bus_sendFrame(NODE_ADDRESS, (uint8_t *)p, sizeof(*p));
+    uint8_t *msg = p;
+    aes_encrypt(msg);
+    bus_sendFrame(NODE_ADDRESS, msg, sizeof(*p));
 }
 
 void bus_sendAck(void)
