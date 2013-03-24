351b81f4093fcd6b2abf0a99136546a550a068d8
schneider <schneider@blinkenlichts.net>
2013-03-24 04:09:40 +0100

    lc: debonce a single buttons

--- software/lc/buttons.c
+++ software/lc/buttons.c
@@ -1,11 +1,61 @@
 #include "buttons.h"
+#include "pinutils.h"
+
+static uint8_t buttons_red_debounce;
+static bool buttons_red_state;
+
+static uint8_t buttons_pending;
+
+void buttons_init(void)
+{
+    DDR_CONFIG_IN(BUTTON_RED);
+    PIN_SET(BUTTON_RED);
+    buttons_pending = 0;
+    buttons_red_state = false;
+    buttons_red_debounce = 0;
+}
 
 uint8_t buttons_getPendingButtons(void)
 {
-    return 0;
+    return buttons_pending;
 }
 
-void buttons_clearButtons(uint8_t buttons)
+void buttons_clearPendingButtons(uint8_t buttons)
 {
+    buttons_pending &= ~(buttons);
 }
 
+bool buttons_getButtonState(buttons_button_t button)
+{
+    switch( button ){
+        case BUTTON_RED:
+            return !PIN_HIGH(BUTTON_RED);
+        break;
+    }
+    return false;
+}
+
+void buttons_tick(void)
+{
+    //TODO: generalize for more buttons
+    if( buttons_getButtonState(BUTTON_RED) ){
+        if( buttons_red_state == false ){
+            if( buttons_red_debounce++ > 100 ){
+                buttons_red_state = true;
+                buttons_red_debounce = 0;
+                buttons_pending |= BUTTON_RED;
+            }
+        }else{
+            buttons_red_debounce = 0;
+        }
+    }else{
+        if( buttons_red_state == true ){
+            if( buttons_red_debounce++ > 100 ){
+                buttons_red_state = false;
+                buttons_red_debounce = 0;
+            }
+        }else{
+            buttons_red_debounce = 0;
+        }
+    }
+}
--- software/lc/buttons.h
+++ software/lc/buttons.h
@@ -1,7 +1,18 @@
 #ifndef BUTTONS_H_
 #define BUTTONS_H_
 #include <stdint.h>
+#include <stdbool.h>
 
+#define BUTTON_RED_PORT              C
+#define BUTTON_RED_PIN               1
+
+typedef enum {
+    BUTTON_RED = 1
+} buttons_button_t;
+
+void buttons_init(void);
 uint8_t buttons_getPendingButtons(void);
-void buttons_clearButtons(uint8_t buttons);
+void buttons_clearPendingButtons(uint8_t buttons);
+bool buttons_getButtonState(buttons_button_t button);
+
 #endif
