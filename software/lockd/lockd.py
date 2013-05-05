e7fc73e3774e09b88fc26ff4e0804f1eca7d3a32
Tobias Schneider <schneider@blinkenlichts.net>
2013-05-05 02:48:52 +0200

    lockd: use DoorLogic in the main loop

--- software/lockd/lockd.py
+++ software/lockd/lockd.py
@@ -11,6 +11,7 @@ import logging
 import logging.handlers
 import traceback
 from announce import Announcer
+from doorlogic import DoorLogic
 
 config = ConfigParser.RawConfigParser()
 config.read(sys.argv[1])
@@ -37,13 +38,15 @@ try:
     announcer = Announcer(False)
 
     ser = serialinterface.SerialInterface(serialdevice, baudrate, timeout=.1)
-    command_queue = Queue.Queue()
+    input_queue = Queue.Queue()
 
-    udpcommand = UDPCommand('127.0.0.1', 2323, command_queue)
+    udpcommand = UDPCommand('127.0.0.1', 2323, input_queue)
 
     doors = {}
 
     master = None
+
+    logic = doorlogic.DoorLogic()
 
     for section in config.sections():
         if config.has_option(section, 'type'):
@@ -63,19 +66,24 @@ try:
                 logger.debug('Adding door "%s"'%section)
                 door = Door(name, address, txseq, rxseq, key, ser, initial_unlock)
                 doors[address] = door
+                logic.add_door(door)
             else:
                 logger.warning('Unknown entry type "%s"', t)
         elif section == 'Master Controller':
             txseq = int(config.get(section, 'txsequence'))
             rxseq = int(config.get(section, 'rxsequence'))
             key = config.get(section, 'key')
-            master = MasterController('0', txseq, rxseq, key, ser, command_queue) 
+            master = MasterController('0', txseq, rxseq, key, ser, input_queue) 
     
     if master == None:
         logger.error('Please specify a master controller')
         sys.exit(1)
 
-    command_queue.put('announce_closed')
+    input_queue.put({'origin_name': 'init',
+                     'origin_type': DoorLogic.Origin.INTERNAL,
+                     'input_name': '',
+                     'input_type': DoorLogic.Input.COMMAND,
+                     'input_value': 'down'})
     
     while True:
         timeout = False
@@ -91,28 +99,11 @@ try:
             elif address == '0':
                 master.update(message)
 
-        if not command_queue.empty():
-            command = command_queue.get()
-            logger.info("-------------Received new command: %s----------------"%command)
-            if command == 'unlock':
-                for door in [door for door in doors.values() if door.initial_unlock == True]:
-                    logger.info("Unlocking %s"%door.name)
-                    door.unlock(relock_timeout=5)
-            elif command == 'lock':
-                for door in doors.values():
-                    door.lock()
-                command_queue.put('announce_closed')
-            elif command == 'announce_open':
-                announcer.announce_open()
-                master.announce_open()
-            elif command == 'announce_closed':
-                announcer.announce_closed()
-                master.announce_closed()
-            elif command == 'toggle_announce':
-                if announcer.open:
-                    command_queue.put('announce_closed')
-                else:
-                    command_queue.put('announce_open')
+        if not input_queue.empty():
+            input = input_queue.get()
+            logic.policy(input['origin_name'], input['origin_type'],
+                         input['input_name'], input['input_type'], 
+                         input['input_value'])
         for d in doors:
             doors[d].tick()
         master.tick()
