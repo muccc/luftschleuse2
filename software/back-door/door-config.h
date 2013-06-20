eb33cec51b613d9e9fcf9de7a22564de449fdbd3
Tobias Schneider <schneider@blinkenlichts.net>
2013-06-20 23:59:40 +0200

    separated the config for the two doors

---
+++ software/back-door/door-config.h
@@ -0,0 +1,11 @@
+#ifndef DOOR_CONFIG_H
+#define DOOR_CONFIG_H
+
+// Back door is DOOR_MODEL_2
+#define DOOR_MODEL  DOOR_MODEL_2
+
+// Back door has address 'B'
+#define NODE_ADDRESS    'B'
+
+#endif
+
