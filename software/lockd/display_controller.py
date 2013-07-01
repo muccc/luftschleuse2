a60573904f07a093c5480aef1acd78a47b3c7f02
Tobias Schneider <schneider@blinkenlichts.net>
2013-07-01 18:33:16 +0200

    added clear() and update() methods to DisplayController

--- software/lockd/display_controller.py
+++ software/lockd/display_controller.py
@@ -12,14 +12,20 @@ class DisplayController:
         self._large_font_size = 16
         self._small_font = ImageFont.truetype('eigenbau.ttf', self._small_font_size)
         self._large_font = ImageFont.truetype('eigenbau.ttf', self._large_font_size)
-        self._display.paste((0,0,0))
+        self.clear()
         self.render_large((10,70/2-16), 'Booting...', 'white');
-        self._display.update()
+        self.update()
     
     def render_small(self, xy, text, color):
         self._draw.text((xy[0]+1, xy[1]), text, color, font=self._small_font)
         
     def render_large(self, xy, text, color):
         self._draw.text((xy[0]+1, xy[1]), text, color, font=self._large_font)
+
+    def clear(self):
+        self._display.paste((0,0,0))
+
+    def update(self):
+        self._display.update()
 
 
