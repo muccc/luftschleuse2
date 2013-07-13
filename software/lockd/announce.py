90d487961d577654427486b2d7ec0d61585ed888
Tobias Schneider <schneider@blinkenlichts.net>
2013-07-13 23:56:54 +0200

    Fixes #18: Host and port of the status receiver can be configured

--- software/lockd/announce.py
+++ software/lockd/announce.py
@@ -2,17 +2,17 @@ import time
 import socket
 
 class Announcer:
-    def __init__(self):
+    def __init__(self, host, port):
         self.timestamp = time.time()
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind(('0.0.0.0', 2080))
-        self.target = ('83.133.178.75', 2080)
-        #self.target = ('83.133.178.69', 2081)
-        self.message = 'fnord'
+        self.target = (host, port)
+        self.message = 'unknown'
 
     def tick(self):
         if time.time() - self.timestamp > 1:
             self.announce()
+            self.timestamp = time.time()
 
     def announce(self):
         self.sock.sendto(self.message, self.target)
