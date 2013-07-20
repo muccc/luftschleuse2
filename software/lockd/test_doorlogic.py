58965b542764ad1468adb054204d7dfa6d50b656
Tobias Schneider <schneider@blinkenlichts.net>
2013-07-21 00:47:07 +0200

    doorlogic test stub

---
+++ software/lockd/test_doorlogic.py
@@ -0,0 +1,13 @@
+import random
+import unittest
+import mock
+import doorlogic
+
+class DoorLogicTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
