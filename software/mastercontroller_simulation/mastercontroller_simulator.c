1b4b210577c48a5f978b0a2e348b4a2ecae91555
Tobias Schneider <schneider@blinkenlichts.net>
2013-07-28 16:48:26 +0200

    reduced cpu load

--- software/mastercontroller_simulation/mastercontroller_simulator.c
+++ software/mastercontroller_simulation/mastercontroller_simulator.c
@@ -3,6 +3,8 @@
 
 #include <ncurses.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <stdint.h>
 
 int main()
 {	
@@ -12,9 +14,13 @@ int main()
     mastercontroller_init();
 
     printf("Main Loop...\n");
+    uint8_t i = 0;
     while(1) {
+        for(i = 0; i < 5; i++) { 
+            mastercontroller_process();
+            usleep(200);
+        }
         mastercontroller_tick();
-        mastercontroller_process();
     }
     //cli_exit();
 	return 0;
