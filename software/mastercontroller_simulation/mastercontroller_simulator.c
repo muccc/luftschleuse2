c7829587e0729438320560b0d3008ad7810fb878
Tobias Schneider <schneider@blinkenlichts.net>
2013-07-28 16:19:38 +0200

    added native delay operations

--- software/mastercontroller_simulation/mastercontroller_simulator.c
+++ software/mastercontroller_simulation/mastercontroller_simulator.c
@@ -2,13 +2,16 @@
 #include "cli.h"
 
 #include <ncurses.h>
+#include <stdio.h>
 
 int main()
 {	
     //cli_init();
 
+    printf("Init..\n");
     mastercontroller_init();
 
+    printf("Main Loop...\n");
     while(1) {
         mastercontroller_tick();
         mastercontroller_process();
--- software/mastercontroller_simulation/util/delay.h
+++ software/mastercontroller_simulation/util/delay.h
@@ -1,7 +1,8 @@
 #ifndef DELAY_H_
 #define DELAY_H_
+#include <unistd.h>
 
-#define _delay_us(x)
-#define _delay_ms(x)
+#define _delay_us(x) usleep(x)
+#define _delay_ms(x) do{ int i = x; while(i--) usleep(1000);}while(0)
 
 #endif
